28a99bb986e8da8f15e482b47e374cf6dcd94c42
Mikhail Proniushkin <mikhailpro4@gmail.com>
2017-12-21 17:10:52 -0500

    everything works

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+	googleClientID:
+		"269667962102-7k5ll15ar2667g2pusj80jvqq0tb45lm.apps.googleusercontent.com",
+	googleClientSecret: "hwiTulLth29lHWmJ1btMEv60",
+	mongoURI: "mongodb://mikepro:Deepmind12$@ds049486.mlab.com:49486/hypedna",
+	cookieKey: "hype"
+};
